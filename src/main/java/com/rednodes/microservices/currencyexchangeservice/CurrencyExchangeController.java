89a0a9d6dd26ab904dc78a6274f0181dd1df2703
ngel rojas <aomarrojasp@gmail.com>
2023-03-23 11:02:16 -0300

    add: adding url for exchange

---
+++ src/main/java/com/rednodes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,16 @@
+package com.rednodes.microservices.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(65));
+    }
+}
