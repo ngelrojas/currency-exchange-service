05a616bc17f7590bad433a4986ca64fe1525bb60
ngel rojas <aomarrojasp@gmail.com>
2023-03-25 16:29:54 -0300

    add: sql script, added in h2 database

--- src/main/java/com/rednodes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/rednodes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,12 +1,35 @@
 package com.rednodes.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name="currency_from")
     private String from;
+
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
+
+    private String environment;
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+
+    public CurrencyExchange() {
+    }
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
         this.id = id;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id, conversion_multiple, currency_from, currency_to )
+values(10001, 65, 'USD', 'INR');
+
+insert into currency_exchange(id, conversion_multiple, currency_from, currency_to )
+values(10002, 75, 'EUR', 'INR');
+
+insert into currency_exchange(id, conversion_multiple, currency_from, currency_to )
+values(10003, 100, 'AUD', 'INR');
