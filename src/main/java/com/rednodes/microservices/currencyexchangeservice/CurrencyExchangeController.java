a83a81fb10b8d5873f2727f664ff3a7447fc4083
ngel rojas <aomarrojasp@gmail.com>
2023-03-28 15:11:06 -0300

    adding jpa-v2

--- src/main/java/com/rednodes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/rednodes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,11 +12,19 @@ import java.math.BigDecimal;
 public class CurrencyExchangeController {
 
     @Autowired
+    private CurrencyExchangeRepository repository;
+    @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(65));
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
+
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
